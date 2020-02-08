b6278dad6666aa71cb96631869af2cd29459aa0e
Łukasz Godlewski <AronieroDYami@gmail.com>
2020-02-08 13:05:40 +0100

    Adding dinnerMonth and teaMonth to the stickyNotes

--- dinner-counter/stickyNotesToPrint/stickyNotes.js
+++ dinner-counter/stickyNotesToPrint/stickyNotes.js
@@ -2,11 +2,13 @@
 
 
 
-
 const date = document.querySelector('.payDate');
 
 
 
+
+
+// Listening for change in "Month for payment" field
 date.addEventListener('change', (e) => {
   const currDate = e.target.value;
   const year = currDate.slice(0,4);
@@ -23,30 +25,43 @@ date.addEventListener('change', (e) => {
   location.reload();
 })
 
+// Listening for change in "Month for dinner" field
+  let dinnerMonth = document.querySelector('.dinnerMonth');
+  dinnerMonth.addEventListener('change', (e) => {
+    localStorage.setItem('dinnerMonth', JSON.stringify(e.target.value));
+    location.reload();
+  })
+
+// Listening for change in "Month for tea" field
+  let teaMonth = document.querySelector('.teaMonth');
+  teaMonth.addEventListener('change', (e) => {
+    localStorage.setItem('teaMonth', JSON.stringify(e.target.value));
+    location.reload();
+  })
 
 
-
-  let peopleArr = [];
-  for (var key in localStorage) {
+// Getting people from localstorage
+let peopleArr = [];
+for (var key in localStorage) {
     peopleArr.push(JSON.parse(localStorage.getItem(key)))
-  }
-  console.log(peopleArr);
-
+}
+  // Sorting people by classNum
   function sortingArr(arr) {
-    const filteredPeople = arr.filter((person) =>
-      person !== null
-    ).filter((person) =>
-      person.hasOwnProperty('name') ? person : ''
-    )
-    // console.log(filteredPeople);
+      const filteredPeople = arr.filter((person) =>
+        person !== null
+      ).filter((person) =>
+        person.hasOwnProperty('name') ? person : ''
+      )
+      // console.log(filteredPeople);
 
 
-    return filteredPeople.sort((a, b) => {
-      // console.log(parseInt(a.classNum), parseInt(b.classNum));
-      return parseFloat(a.classNum) < parseFloat(b.classNum) ? -1 : 1
-    })
+      return filteredPeople.sort((a, b) => {
+        // console.log(parseInt(a.classNum), parseInt(b.classNum));
+        return parseFloat(a.classNum) < parseFloat(b.classNum) ? -1 : 1
+      })
   }
 
+// Creating Sticky Notes and outputing them into HTML
 sortingArr(peopleArr).map((person) => {
   // console.log(person);
   if (person == null || person.day ) {
@@ -55,8 +70,12 @@ sortingArr(peopleArr).map((person) => {
     const table = document.querySelector('.container');
     const stickyNote = document.createElement('div');
     stickyNote.classList.add('stickyNote');
+    // Getting month for payment, dinnerMonth and teaMonth from LS
     const date = JSON.parse(localStorage.getItem('date'));
+    const dinnerMonth = JSON.parse(localStorage.getItem('dinnerMonth'))
+    const teaMonth = JSON.parse(localStorage.getItem('teaMonth'))
 
+    console.log(teaMonth);
 
     const rowContent = `
         <h4>Informacja dla ${person.classNum === '-1' ? 'nauczycieli' : `rodziców`}</h4>
@@ -64,9 +83,9 @@ sortingArr(peopleArr).map((person) => {
         <div class="person-container">
           <div class="person ">${person.surname}  ${person.name}</div>
           <div class="classNum">${person.classNum === '-1' ? '' : `Klasa ${person.classNum}`}</div>
-          <div class="dinners">Obiady: </div>
+          <div class="dinners">Obiady (${dinnerMonth}): </div>
           <div class="dinnersValue">${person.dinnerQuantity} * ${person.dinnerPrice}zł = ${person.dinnerValue} zł</div>
-          <div class="teas">Herbaty: </div>
+          <div class="teas">Herbaty (${teaMonth}): </div>
           <div class="teasValue">${person.teaQuantity} * ${person.teaPrice}zł = ${person.teaValue} zł</div>
           <div class="total">Razem: </div>
           <div class="totalValue">${person.totalValue} zł</div>
